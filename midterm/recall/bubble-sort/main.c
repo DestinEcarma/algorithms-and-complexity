7a76e9acfeac59e185e1f998fe27f6d3d8eb1f1a
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-22 20:08:39 +0800

    add: `midterm/recall/bubble-sort/main.c`

---
+++ midterm/recall/bubble-sort/main.c
@@ -0,0 +1,28 @@
+#include "helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b) {
+	return *(int *)a > *(int *)b;
+}
+
+void print_int(const void *value) {
+	printf("%d", *(int *)value);
+}
+
+int main() {
+	int arr[] = {9, 5, 10, 7, 3, 2, 6, 4, 1};
+	int len = sizeof(arr) / sizeof(int);
+
+	printf("Initial: ");
+	print_arr(arr, len, sizeof(int), print_int);
+
+	int *sorted = bubble_sort(arr, len, sizeof(int), cmp);
+
+	printf("Sorted: ");
+	print_arr(sorted, len, sizeof(int), print_int);
+
+	free(sorted);
+
+	return 0;
+}
