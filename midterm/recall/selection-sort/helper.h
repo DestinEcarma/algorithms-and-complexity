ea30ae94ad8336a7cb15a524e9208c8f3a4d19ea
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-22 21:55:23 +0800

    feat: add `selection_sort_reverse`

--- midterm/recall/selection-sort/helper.h
+++ midterm/recall/selection-sort/helper.h
@@ -6,6 +6,9 @@
 void *selection_sort(void *arr, size_t len, size_t byte_size,
 					 int (*cmp)(const void *, const void *));
 
+void *selection_sort_reverse(void *arr, size_t len, size_t byte_size,
+							 int (*cmp)(const void *, const void *));
+
 void print_arr(void *arr, size_t len, size_t byte_size,
 			   void (*print)(const void *));
 
