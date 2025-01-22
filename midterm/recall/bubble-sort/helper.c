024ed3622ea346353a4db734d3062a998466d3ee
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-22 20:42:28 +0800

    refactor: moved `temp` outside of the loop

--- midterm/recall/bubble-sort/helper.c
+++ midterm/recall/bubble-sort/helper.c
@@ -6,6 +6,7 @@
 void *bubble_sort(void *arr, size_t len, size_t byte_size,
 				  int (*cmp)(const void *, const void *)) {
 	void *sorted = malloc(len * byte_size);
+	void *temp = malloc(byte_size);
 
 	memcpy(sorted, arr, len * byte_size);
 
@@ -19,17 +20,16 @@ void *bubble_sort(void *arr, size_t len, size_t byte_size,
 			size_t next = curr + byte_size;
 
 			if (cmp(sorted + curr, sorted + next)) {
-				void *temp = malloc(byte_size);
-
 				memcpy(temp, sorted + curr, byte_size);
 				memcpy(sorted + curr, sorted + next, byte_size);
 				memcpy(sorted + next, temp, byte_size);
 				swapped = 1;
 
-				free(temp);
 			}
 		}
 	}
+
+	free(temp);
 
 	return sorted;
 }
