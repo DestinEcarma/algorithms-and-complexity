698619105fea88f928630102f21eedc7135c701b
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-22 22:13:35 +0800

    feat: added docs to functions

--- midterm/recall/bubble-sort/helper.h
+++ midterm/recall/bubble-sort/helper.h
@@ -3,9 +3,13 @@
 
 #include <stddef.h>
 
+/// This function is a generic bubble sort implementation.
+/// Be it a integer, float, or even a object/structure,
+/// this function can sort those array.
 void *bubble_sort(void *arr, size_t len, size_t byte_size,
 				  int (*cmp)(const void *, const void *));
 
+/// A function to display an array onto the stdout/console.
 void print_arr(void *arr, size_t len, size_t byte_size,
 			   void (*print)(const void *));
 
--- midterm/recall/selection-sort/helper.h
+++ midterm/recall/selection-sort/helper.h
@@ -3,12 +3,19 @@
 
 #include <stddef.h>
 
+/// This function is a generic selection sort implementation.
+/// Be it a integer, float, or even a object/structure,
+/// this function can sort those array.
 void *selection_sort(void *arr, size_t len, size_t byte_size,
 					 int (*cmp)(const void *, const void *));
 
+/// This function is a generic insertion sort implementation.
+/// This function will have the same result as `selection_sort`.
+/// The only difference is that this function will sort the array from the rear.
 void *selection_sort_reverse(void *arr, size_t len, size_t byte_size,
 							 int (*cmp)(const void *, const void *));
 
+/// A function to display an array onto the stdout/console.
 void print_arr(void *arr, size_t len, size_t byte_size,
 			   void (*print)(const void *));
 
