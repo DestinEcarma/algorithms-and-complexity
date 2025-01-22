351ca7b49bdbf36c92c4a680397a6668b56dad16
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-22 20:19:56 +0800

    add: `midterm/recall/insertion-sort/helper.*`

---
+++ midterm/recall/insertion-sort/helper.c
@@ -0,0 +1,47 @@
+#include "helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *insertion_sort(void *arr, size_t len, size_t byte_size,
+					 int (*cmp)(const void *, const void *)) {
+	void *sorted = malloc(len * byte_size);
+	void *temp = malloc(byte_size);
+
+	memcpy(sorted, arr, len * byte_size);
+
+	for (size_t i = 1; i < len; i++) {
+		memcpy(temp, sorted + i * byte_size, byte_size);
+
+		size_t j = 0;
+
+		while (j < i && cmp(temp, sorted + j * byte_size)) {
+			j++;
+		}
+
+		for (size_t k = i; k > j; k--) {
+			memcpy(sorted + k * byte_size, sorted + (k - 1) * byte_size,
+				   byte_size);
+		}
+
+		memcpy(sorted + j * byte_size, temp, byte_size);
+	}
+
+	free(temp);
+
+	return sorted;
+}
+
+void print_arr(void *arr, size_t len, size_t byte_size,
+			   void (*print)(const void *)) {
+	printf("[");
+
+	for (size_t i = 0; i < len; i++) {
+		print(arr + i * byte_size);
+
+		if (i < len - 1)
+			printf(", ");
+	}
+
+	printf("]\n");
+}
---
+++ midterm/recall/insertion-sort/helper.h
@@ -0,0 +1,12 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <stddef.h>
+
+void *insertion_sort(void *arr, size_t len, size_t byte_size,
+					 int (*cmp)(const void *, const void *));
+
+void print_arr(void *arr, size_t len, size_t byte_size,
+			   void (*print)(const void *));
+
+#endif
