5b623cd2702b6b089d1643db35017816452bcc63
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-24 01:47:59 +0800

    feat: added `cmp_int_l` for less than comparision

--- common/cmp.h
+++ common/cmp.h
@@ -1,6 +1,11 @@
 #ifndef CMP_H
 #define CMP_H
 
+/// Compare `a` is less than `b` integers.
+static inline int cmp_int_l(const void *a, const void *b) {
+	return *(int *)a < *(int *)b;
+}
+
 /// Compare `a` is greater than `b` integers.
 static inline int cmp_int_g(const void *a, const void *b) {
 	return *(int *)a > *(int *)b;
