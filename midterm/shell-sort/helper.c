ffe8c5ddf7112a522690c7e3e44c45331f726771
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-23 23:10:35 +0800

    add: `midterm/shell-sort/helper.*`

---
+++ midterm/shell-sort/helper.c
@@ -0,0 +1,44 @@
+#include "helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *shell_sort(void *arr, size_t len, size_t byte_size,
+				 int (*cmp)(const void *, const void *)) {
+	void *sorted = malloc(len * byte_size);
+	void *temp = malloc(byte_size);
+
+	memcpy(sorted, arr, len * byte_size);
+
+	for (size_t gap = len / 2; gap > 0; gap /= 2) {
+		for (size_t i = gap; i < len; i++) {
+			memcpy(temp, sorted + i * byte_size, byte_size);
+
+			size_t j = i;
+
+			while (j >= gap && cmp(sorted + (j - gap) * byte_size, temp)) {
+				memcpy(sorted + j * byte_size, sorted + (j - gap) * byte_size,
+					   byte_size);
+				j -= gap;
+			}
+
+			memcpy(sorted + j * byte_size, temp, byte_size);
+		}
+	}
+
+	return sorted;
+}
+
+void print_arr(void *arr, size_t len, size_t byte_size,
+			   void (*print)(const void *)) {
+	printf("[");
+
+	for (size_t i = 0; i < len; i++) {
+		print(arr + i * byte_size);
+
+		if (i < len - 1)
+			printf(", ");
+	}
+
+	printf("]\n");
+}
---
+++ midterm/shell-sort/helper.h
@@ -0,0 +1,15 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <stddef.h>
+
+/// This function is a generic shell sort implementation.
+/// Be it a integer, float, or even a object/structure,
+/// this function can sort those array.
+void *shell_sort(void *arr, size_t len, size_t byte_size,
+				 int (*cmp)(const void *, const void *));
+
+/// A function to display an array onto the stdout/console.
+void print_arr(void *arr, size_t len, size_t byte_size,
+			   void (*print)(const void *));
+#endif
