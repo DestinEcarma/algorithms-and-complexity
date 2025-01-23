cd430ab466bccbc155c0e397337468061624d0f2
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-01-24 01:46:30 +0800

    fix: `selection-sort` invalid algorithm

--- midterm/recall/selection-sort/README.md
+++ midterm/recall/selection-sort/README.md
@@ -8,12 +8,12 @@ This is my implementation of the selection sort algorithm in C.
 let A be the array to be sorted
 let n be the length of A
 
-for i = 1 to n - 1 do
-    let min i = i - 1
+for i = 0 to n - 1 do
+    let min i = i
 
-    for j = i to n - 1 do
+    for j = i + 1 to n - 1 do
         if A[j] < A[min i] then
             min i = j
 
-    swap A[i - 1] with A[min i]
+    swap A[i] with A[min i]
 ```
--- midterm/recall/selection-sort/helper.c
+++ midterm/recall/selection-sort/helper.c
@@ -11,18 +11,17 @@ void *selection_sort(void *arr, size_t len, size_t byte_size,
 
 	memcpy(sorted, arr, len * byte_size);
 
-	for (size_t i = 1; i < len; i++) {
+	for (size_t i = 0; i < len; i++) {
 		size_t min_i = i;
-		size_t k = i - 1;
 
-		for (size_t j = i; j < len; j++) {
+		for (size_t j = i + 1; j < len; j++) {
 			if (cmp(sorted + j * byte_size, sorted + min_i * byte_size)) {
 				min_i = j;
 			}
 		}
 
-		memcpy(temp, sorted + k * byte_size, byte_size);
-		memcpy(sorted + k * byte_size, sorted + min_i * byte_size, byte_size);
+		memcpy(temp, sorted + i * byte_size, byte_size);
+		memcpy(sorted + i * byte_size, sorted + min_i * byte_size, byte_size);
 		memcpy(sorted + min_i * byte_size, temp, byte_size);
 	}
 
@@ -38,18 +37,17 @@ void *selection_sort_reverse(void *arr, size_t len, size_t byte_size,
 
 	memcpy(sorted, arr, len * byte_size);
 
-	for (size_t i = len - 2; i != ULONG_MAX; i--) {
+	for (size_t i = len - 1; i != ULONG_MAX; i--) {
 		size_t max_i = i;
-		size_t k = i + 1;
 
-		for (size_t j = i; j != ULONG_MAX; j--) {
+		for (size_t j = i - 1; j != ULONG_MAX; j--) {
 			if (!cmp(sorted + j * byte_size, sorted + max_i * byte_size)) {
 				max_i = j;
 			}
 		}
 
-		memcpy(temp, sorted + k * byte_size, byte_size);
-		memcpy(sorted + k * byte_size, sorted + max_i * byte_size, byte_size);
+		memcpy(temp, sorted + i * byte_size, byte_size);
+		memcpy(sorted + i * byte_size, sorted + max_i * byte_size, byte_size);
 		memcpy(sorted + max_i * byte_size, temp, byte_size);
 	}
 
