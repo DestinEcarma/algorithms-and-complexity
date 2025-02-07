56a592c6a74e8c99858fc8dbe906d7dd873dee07
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-08 04:55:06 +0800

    refactor: remove unused fields on `MergeMetaData`

--- midterm/merge-sort/helper.c
+++ midterm/merge-sort/helper.c
@@ -6,10 +6,7 @@
 #define MIN(a, b) (a < b ? a : b)
 
 typedef struct MergeMetaData {
-	size_t left_start;
-	size_t left_size;
-	size_t right_start;
-	size_t right_size;
+	size_t mid;
 	size_t length;
 } MergeMeta;
 
@@ -17,15 +14,17 @@ void merge(void *arr, void *temp, MergeMeta meta, size_t byte_size,
 		   int (*cmp)(const void *, const void *)) {
 	memcpy(temp, arr, meta.length * byte_size);
 
+	size_t right_len = meta.length - meta.mid;
+
 	size_t left_idx = 0;
 	size_t right_idx = 0;
 	size_t i = 0;
 
-	while (left_idx < meta.left_size && right_idx < meta.right_size) {
+	while (left_idx < meta.mid && right_idx < right_len) {
 		void *value = NULL;
 
 		void *left_value = temp + left_idx * byte_size;
-		void *right_value = temp + (meta.left_size + right_idx) * byte_size;
+		void *right_value = temp + (meta.mid + right_idx) * byte_size;
 
 		if (cmp(left_value, right_value)) {
 			value = left_value;
@@ -38,32 +37,26 @@ void merge(void *arr, void *temp, MergeMeta meta, size_t byte_size,
 		memcpy(arr + i++ * byte_size, value, byte_size);
 	}
 
-	if (left_idx < meta.left_size) {
+	if (left_idx < meta.mid) {
 		memcpy(arr + i * byte_size, temp + left_idx * byte_size,
-			   byte_size * (meta.left_size - left_idx));
+			   byte_size * (meta.mid - left_idx));
 	}
 
-	if (right_idx < meta.right_size) {
+	if (right_idx < right_len) {
 		memcpy(arr + (left_idx + right_idx) * byte_size,
-			   temp + (meta.left_size + right_idx) * byte_size,
-			   byte_size * (meta.right_size - right_idx));
+			   temp + (meta.mid + right_idx) * byte_size,
+			   byte_size * (right_len - right_idx));
 	}
 }
 
 void _merge_sort_recursion(void *arr, void *temp, size_t len, size_t byte_size,
 						   int (*cmp)(const void *, const void *)) {
 	if (len > 1) {
-		size_t mid = len / 2;
+		MergeMeta meta = {.mid = len / 2, .length = len};
 
-		MergeMeta meta = {.left_start = 0,
-						  .left_size = mid,
-						  .right_start = mid,
-						  .right_size = len - mid,
-						  .length = len};
-
-		_merge_sort_recursion(arr, temp, meta.left_size, byte_size, cmp);
-		_merge_sort_recursion(arr + meta.right_start * byte_size, temp,
-							  meta.right_size, byte_size, cmp);
+		_merge_sort_recursion(arr, temp, meta.mid, byte_size, cmp);
+		_merge_sort_recursion(arr + meta.mid * byte_size, temp,
+							  meta.length - meta.mid, byte_size, cmp);
 
 		merge(arr, temp, meta, byte_size, cmp);
 	}
@@ -92,11 +85,8 @@ void *merge_sort_iterative(const void *arr, size_t len, size_t byte_size,
 
 	for (size_t size = 1; size < len; size *= 2) {
 		for (size_t start = 0; start < len - size; start += size * 2) {
-			MergeMeta meta = {.left_start = start,
-							  .left_size = size,
-							  .right_start = start + size,
-							  .right_size = MIN(size, len - start - size),
-							  .length = MIN(size * 2, len)};
+			MergeMeta meta = {.mid = size,
+							  .length = MIN(size * 2, len - start)};
 
 			merge(sorted + start * byte_size, temp, meta, byte_size, cmp);
 		}
