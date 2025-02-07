2841576e1d5f5bfbac184b4ed4e355f7e80239ee
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-08 02:31:51 +0800

    add: `common/stress-test.h`

---
+++ common/stress-test.h
@@ -0,0 +1,21 @@
+#ifndef STRESS_TEST_H
+#define STRESS_TEST_H
+
+#include <stdlib.h>
+#include <time.h>
+
+static inline unsigned long
+stress_test_sort(const void *arr, size_t len, size_t byte_size, size_t nt,
+				 void *(*sort)(const void *, size_t, size_t,
+							   int (*)(const void *, const void *)),
+				 int (*cmp)(const void *, const void *)) {
+	clock_t start = clock();
+
+	for (size_t _ = 0; _ < nt; _++) {
+		free(sort(arr, len, byte_size, cmp));
+	}
+
+	return (double)(clock() - start) / CLOCKS_PER_SEC * 1000;
+}
+
+#endif
