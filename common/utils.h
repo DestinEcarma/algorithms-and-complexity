9df21f03913dcca16a9d2d3f4ded747b20a7d8fe
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-08 03:55:44 +0800

    add: `common/utils.h`

---
+++ common/utils.h
@@ -0,0 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stddef.h>
+
+static inline void swap(void *a, void *b, size_t byte_size) {
+	unsigned char temp;
+
+	for (size_t i = 0; i < byte_size; i++) {
+		temp = *((unsigned char *)a + i);
+		*((unsigned char *)a + i) = *((unsigned char *)b + i);
+		*((unsigned char *)b + i) = temp;
+	}
+}
+
+#endif
