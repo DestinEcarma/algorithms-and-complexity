0a2efbd25c25499021e141f889b3e98c5fef84bd
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-14 15:44:12 +0800

    feat: check if `sorted` is not null in `shell-sort`

--- midterm/shell-sort/helper.c
+++ midterm/shell-sort/helper.c
@@ -6,27 +6,29 @@
 void *shell_sort(const void *arr, size_t len, size_t byte_size,
 				 int (*cmp)(const void *, const void *)) {
 	void *sorted = malloc(len * byte_size);
-	void *temp = malloc(byte_size);
 
-	memcpy(sorted, arr, len * byte_size);
+	if (sorted != NULL) {
+		memcpy(sorted, arr, len * byte_size);
 
-	for (size_t gap = len / 2; gap > 0; gap /= 2) {
-		for (size_t i = gap; i < len; i++) {
-			memcpy(temp, sorted + i * byte_size, byte_size);
+		unsigned char temp[byte_size];
 
-			size_t j = i;
+		for (size_t gap = len / 2; gap > 0; gap /= 2) {
+			for (size_t i = gap; i < len; i++) {
+				memcpy(temp, sorted + i * byte_size, byte_size);
 
-			while (j >= gap && cmp(sorted + (j - gap) * byte_size, temp)) {
-				memmove(sorted + j * byte_size, sorted + (j - gap) * byte_size,
-						byte_size);
-				j -= gap;
+				size_t j = i;
+
+				while (j >= gap && cmp(sorted + (j - gap) * byte_size, temp)) {
+
+					memmove(sorted + j * byte_size,
+							sorted + (j - gap) * byte_size, byte_size);
+					j -= gap;
+				}
+
+				memcpy(sorted + j * byte_size, temp, byte_size);
 			}
-
-			memcpy(sorted + j * byte_size, temp, byte_size);
 		}
 	}
-
-	free(temp);
 
 	return sorted;
 }
