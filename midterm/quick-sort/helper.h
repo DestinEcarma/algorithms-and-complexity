e79a6f95c19e65ec20b13c6105c629fb5b69ab65
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-14 15:58:06 +0800

    style: fix styling error for some sorting algorithms

--- midterm/quick-sort/helper.h
+++ midterm/quick-sort/helper.h
@@ -3,15 +3,15 @@
 
 #include <stddef.h>
 
-/// This function is a generic quick sort sort implementation
-/// with lomuto's partition method. Be it a integer, float, or
-/// even a object/structure, this function can sort those array.
+/// This function is a generic quick sort sort implementation with lomuto's
+/// partition method. Be it a integer, float, or even a object/structure, this
+/// function can sort those array.
 void *quick_sort_lomuto(const void *arr, size_t len, size_t byte_size,
 						int (*cmp)(const void *, const void *));
 
-/// This function is a generic quick sort sort implementation
-/// with hoare's partition method. Be it a integer, float, or
-/// even a object/structure, this function can sort those array.
+/// This function is a generic quick sort sort implementation with hoare's
+/// partition method. Be it a integer, float, or even a object/structure, this
+/// function can sort those array.
 void *quick_sort_hoare(const void *arr, size_t len, size_t byte_size,
 					   int (*cmp_l)(const void *, const void *));
 
