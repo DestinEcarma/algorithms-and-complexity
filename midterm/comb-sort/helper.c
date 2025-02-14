07aab09f9aacda1cd70289fb56b321132680eb2a
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-14 15:46:25 +0800

    feat: check if `sorted` is not null in `comb-sort`

--- midterm/comb-sort/helper.c
+++ midterm/comb-sort/helper.c
@@ -9,24 +9,27 @@ void *comb_sort(const void *arr, size_t len, size_t byte_size,
 				int (*cmp)(const void *, const void *)) {
 	void *sorted = malloc(len * byte_size);
 
-	memcpy(sorted, arr, len * byte_size);
+	if (sorted != NULL) {
+		memcpy(sorted, arr, len * byte_size);
 
-	size_t gap = len;
-	bool swapped = true;
+		size_t gap = len;
+		bool swapped = true;
 
-	while (gap > 1 || swapped) {
-		swapped = false;
-		gap = gap / 1.3;
+		while (gap > 1 || swapped) {
+			swapped = false;
+			gap = gap / 1.3;
 
-		if (gap < 1) {
-			gap = 1;
-		}
+			if (gap < 1) {
+				gap = 1;
+			}
 
-		for (size_t i = 0; i < len - gap; i++) {
-			if (cmp(sorted + i * byte_size, sorted + (i + gap) * byte_size)) {
-				swap(sorted + i * byte_size, sorted + (i + gap) * byte_size,
-					 byte_size);
-				swapped = true;
+			for (size_t i = 0; i < len - gap; i++) {
+				if (cmp(sorted + i * byte_size,
+						sorted + (i + gap) * byte_size)) {
+					swap(sorted + i * byte_size, sorted + (i + gap) * byte_size,
+						 byte_size);
+					swapped = true;
+				}
 			}
 		}
 	}
