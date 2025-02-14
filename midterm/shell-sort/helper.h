3cd023561f88ecbacc6b86d691aab089baa51077
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-14 15:43:44 +0800

    style: fix styling error for `shell-sort/helper.h`

--- midterm/shell-sort/helper.h
+++ midterm/shell-sort/helper.h
@@ -3,9 +3,8 @@
 
 #include <stddef.h>
 
-/// This function is a generic shell sort implementation.
-/// Be it a integer, float, or even a object/structure,
-/// this function can sort those array.
+/// This function is a generic shell sort implementation. Be it a integer,
+/// float, or even a object/structure, this function can sort those array.
 void *shell_sort(const void *arr, size_t len, size_t byte_size,
 				 int (*cmp)(const void *, const void *));
 
