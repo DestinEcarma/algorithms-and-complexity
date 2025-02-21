8f1120e35ba23d7b5cd71b801a90f22cd5cf73a0
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-02-22 02:10:28 +0800

    feat: added greater or equal to and less than or equal to integer function to `cmp`

--- common/cmp.h
+++ common/cmp.h
@@ -11,4 +11,14 @@ static inline int cmp_int_g(const void *a, const void *b) {
 	return *(int *)a > *(int *)b;
 }
 
+/// Compare `a` is less than or equal to `b` integers.
+static inline int cmp_int_leq(const void *a, const void *b) {
+	return *(int *)a <= *(int *)b;
+}
+
+/// Compare `a` is greater than or equal to `b` integers.
+static inline int cmp_int_geq(const void *a, const void *b) {
+	return *(int *)a >= *(int *)b;
+}
+
 #endif
