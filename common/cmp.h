48db475e768fbb7b1a58f9a8de507e7b953e7d05
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-03-19 07:01:34 +0800

    feat: added operations for `doubly-linked-list` to `cmp.h`

--- common/cmp.h
+++ common/cmp.h
@@ -1,6 +1,7 @@
 #ifndef CMP_H
 #define CMP_H
 
+#include "doubly-linked-list.h"
 #include "linked-list.h"
 
 /// Compare `a` is less than `b` integers.
@@ -40,6 +41,26 @@ static inline int cmp_ll_int_leq(const Node *a, const Node *b) {
 
 /// Compare `a` is greater than or equal to `b` Node integers.
 static inline int cmp_ll_int_geq(const Node *a, const Node *b) {
+	return (int)a->value[0] >= (int)b->value[0];
+}
+
+/// Compare `a` is less than `b` doubly linked list integers.
+static inline int cmp_dll_int_l(const DNode *a, const DNode *b) {
+	return (int)a->value[0] < (int)b->value[0];
+}
+
+/// Compare `a` is greater than `b` doubly linked list integers.
+static inline int cmp_dll_int_g(const DNode *a, const DNode *b) {
+	return (int)a->value[0] > (int)b->value[0];
+}
+
+/// Compare `a` is less than or equal to `b` doubly linked list integers.
+static inline int cmp_dll_int_leq(const DNode *a, const DNode *b) {
+	return (int)a->value[0] <= (int)b->value[0];
+}
+
+/// Compare `a` is greater than or equal to `b` doubly linked list integers.
+static inline int cmp_dll_int_geq(const DNode *a, const DNode *b) {
 	return (int)a->value[0] >= (int)b->value[0];
 }
 
