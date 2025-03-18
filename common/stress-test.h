dcfe9fb9625d7d97b50a6f21605990867047220a
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-03-18 15:19:38 +0800

    feat: add `stress_test_sort_int` function

--- common/stress-test.h
+++ common/stress-test.h
@@ -23,4 +23,21 @@ stress_test_sort(const void *arr, size_t len, size_t byte_size, size_t nt,
 	return (double)(clock() - start) / CLOCKS_PER_SEC * 1000;
 }
 
+/// This function is used to stress test a sorting algorithm for integers.
+static inline unsigned long
+stress_test_sort_int(const int *arr, size_t len, size_t nt,
+					 int *(*sort)(const int *, size_t)) {
+	clock_t start = clock();
+
+	for (size_t _ = 0; _ < nt; _++) {
+		int *sorted = sort(arr, len);
+
+		if (sorted != NULL) {
+			free(sorted);
+		}
+	}
+
+	return (double)(clock() - start) / CLOCKS_PER_SEC * 1000;
+}
+
 #endif
