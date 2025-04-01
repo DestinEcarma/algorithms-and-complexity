876c994521441ddcef09f3d2a955dc0f55a45b47
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-04-01 18:03:53 +0800

    add: `finals/common/doubly-node.hpp`

---
+++ finals/common/doubly-node.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T> class DoublyNode {
+  public:
+    T value;
+    DoublyNode<T> *left;
+    DoublyNode<T> *right;
+
+    DoublyNode(T value) : value(value), left(nullptr), right(nullptr) {}
+
+    std::string to_string() {
+        std::string result;
+
+        if (left != nullptr) {
+            result += left->to_string();
+        }
+
+        result += std::to_string(value) + " ";
+
+        if (right != nullptr) {
+            result += right->to_string();
+        }
+
+        return result;
+    }
+};
